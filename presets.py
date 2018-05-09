e7f7863ba2abc479ebcad9bb74366424bc8fed63
KaiZhang <kaizhangbit@gmail.com>
2018-05-09 02:12:03 -0400

    Find a preset making environment WOri-v0 converge.

--- presets.py
+++ presets.py
@@ -1448,3 +1448,19 @@ class TwoOb_NAF(Preset):
 
         self.evaluation_episodes = 3
         self.evaluate_every_x_episodes = 5
+
+class WOri_NAF_SUCCESS(Preset):
+    def __init__(self):
+        Preset.__init__(self, NAF, GymVectorObservation, AdditiveNoiseExploration)
+        self.env.level = 'WOri-v0'
+        self.learning_rate = 0.0007
+        self.num_heatup_steps = 350
+        self.batch_size = 32
+
+        self.evaluation_episodes = 1
+        self.evaluate_every_x_episodes = 5
+
+        self.exploration.initial_noise_variance_percentage = 0.3
+        self.exploration.noise_variance_decay_steps = 3000
+
+        self.agent.num_episodes_in_experience_replay = 10
